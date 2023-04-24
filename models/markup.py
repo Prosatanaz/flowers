cd52cf886085c9a05fd138ff35181a2fa548285d
uZVER <prosatanaz@gmail.com>
2023-04-25 02:31:51 +0300

    add url webapp, refactor index.html

--- models/markup.py
+++ models/markup.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
 def create_start_markup():
     # создаем кнопку
-    start_button = InlineKeyboardButton('Старт', web_app=WebAppInfo(url=''))
+    start_button = InlineKeyboardButton('Старт', web_app=WebAppInfo(url='https://prosatanaz.github.io/flowers/'))
     # создаем клавиатуру и добавляем кнопку
     start_keyboard = InlineKeyboardMarkup().add(start_button)
     return start_keyboard
